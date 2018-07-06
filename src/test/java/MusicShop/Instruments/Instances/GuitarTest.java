255f05f11f92e23699791ff36896cffda6400212
StuartCharters <charters.stuart@gmail.com>
2018-07-06 13:24:37 +0100

    guitar string passing all inherited method tests

--- src/test/java/MusicShop/Instruments/Instances/GuitarTest.java
+++ src/test/java/MusicShop/Instruments/Instances/GuitarTest.java
@@ -5,25 +5,34 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class GuitarTest {
 
+    private Guitar guitar;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        guitar = new Guitar(10.00, 15.00);
     }
 
     @Test
     public void play() {
+        assertEquals("whockah", guitar.play());
     }
 
     @Test
     public void calculateMarkup() {
+        assertEquals(5.00, guitar.calculateMarkup(), 0.001);
     }
 
     @Test
     public void getRetailPrice() {
+        assertEquals(15.00, guitar.getRetailPrice(), 0.001);
     }
 
     @Test
     public void getSuppliersPrice() {
+        assertEquals(10.00, guitar.getSuppliersPrice(), 0.001);
     }
+
 }
--- src/test/java/MusicShop/InstumentAccessories/Instances/GuitarStringTest.java
+++ src/test/java/MusicShop/InstumentAccessories/Instances/GuitarStringTest.java
@@ -1,29 +1,44 @@
 package MusicShop.InstumentAccessories.Instances;
 
+import MusicShop.Instruments.Instances.Guitar;
+import MusicShop.Instruments.Instrument;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class GuitarStringTest {
 
+    private GuitarString guitarString;
+    private Guitar guitar;
+    private ArrayList<Instrument> compatibleInstruments = new ArrayList<>();
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        guitar = new Guitar(10.00, 15.00);
+        compatibleInstruments.add(guitar);
+        guitarString = new GuitarString(2.00, 7.00, compatibleInstruments);
     }
 
     @Test
     public void calculateMarkup() {
+        assertEquals(5.00, guitarString.calculateMarkup(), 0.001);
     }
 
     @Test
     public void getRetailPrice() {
+        assertEquals(7.00, guitarString.getRetailPrice(), 0.001);
     }
 
     @Test
     public void getSuppliersPrice() {
+        assertEquals(2.00, guitarString.getSuppliersPrice(), 0.001);
     }
 
     @Test
     public void getUsedWith() {
+        assertEquals(1, guitarString.getUsedWith().size());
     }
 }
